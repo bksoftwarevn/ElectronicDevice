63b762b797f94e3fe7dbe14ea6bfae534cc67c02
hieuvp98 <hieu.minh9820@gmail.com>
2020-06-01 15:28:26 +0700

    complete BrandDAO

--- src/main/java/com/bksoftware/Main.java
+++ src/main/java/com/bksoftware/Main.java
@@ -10,13 +10,16 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        MyConnection myConnection = new MyConnection();
-//        try {
-//            myConnection.getMyconnection();
-//            BrandDAO dao = new BrandDAO();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        // write your code here
+        MyConnection myConnection = new MyConnection();
+        try {
+            myConnection.getMyconnection();
+            BrandDAO dao = new BrandDAO();
+//            Brand brand = dao.upload(new Brand(0, "Dell", false));
+//            System.out.println(brand.getId());
+               dao.update(new Brand(1,"SpaceX", false));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/bksoftware/dao/BrandDAO.java
+++ src/main/java/com/bksoftware/dao/BrandDAO.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class BrandDAO extends BaseDAO<Brand>{
+public class BrandDAO extends BaseDAO<Brand> {
 
     public BrandDAO() {
         super("brand");
@@ -14,17 +14,29 @@ public class BrandDAO extends BaseDAO<Brand>{
 
     @Override
     public Brand upload(Brand data) throws Exception {
-        String sql = "insert into " + table + " value(null,?,?); select * from "+ table + " where deleted = false";
+        String sql = "insert into " + table + " value(null,?,?);";
         PreparedStatement ps = getPS(sql);
         ps.setString(1, data.getName());
         ps.setBoolean(2, data.isDeleted());
-        ResultSet rs =  ps.executeQuery();
-        rs.last();
-        return getObject(rs);
+        if (ps.executeUpdate() > 0) {
+            String sql2 = "select * from " + table + " where deleted = false";
+            PreparedStatement ps2 = getPS(sql2);
+            ResultSet rs = ps2.executeQuery();
+            rs.last();
+            return getObject(rs);
+        } else
+            return null;
     }
 
     @Override
     public Brand update(Brand data) throws Exception {
+        String sql = "update " + table + " set name = ?, deleted = ? where id = ?";
+        PreparedStatement ps = getPS(sql);
+        ps.setString(1, data.getName());
+        ps.setBoolean(2, data.isDeleted());
+        ps.setInt(3, data.getId());
+        if (ps.executeUpdate() > 0)
+            return data;
         return null;
     }
 
